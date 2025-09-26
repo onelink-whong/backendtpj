0d1fd044a190a89e5f161f0fcb4647e0422b9bee
onelink-whong <seoulwh@gmail.com>
2025-09-26 11:50:48 +0900

    port

--- app.py
+++ app.py
@@ -146,8 +146,8 @@ if __name__ == '__main__':
     print(f"CLOVA API URL: {CLOVA_INVOKE_URL[:50] + '...' if CLOVA_INVOKE_URL else 'None'}")
     print(f"CLOVA SECRET KEY: {'설정됨' if CLOVA_SECRET_KEY else '설정되지 않음'}")
     # 배포용
-    # port=int(os.environ.get('PORT',10000))
-    # app.run(debug=False, host='0.0.0.0', port=port)
+    port=int(os.environ.get('PORT',10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
     # 개발용
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    # app.run(debug=True, host='0.0.0.0', port=8000)
